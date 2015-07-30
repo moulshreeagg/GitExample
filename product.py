75b8771fa1eee72d2b920095a6926d44ad6aa6f8
Moulshree Aggarwal <moulshree.aggarwal@gmail.com>
2015-07-30 22:08:32 +0530

    I made a mistake :(

---
+++ product.py
@@ -0,0 +1,29 @@
+import sys
+
+def main():
+	print "HI"
+	print sys.argv
+	for i in range(1,len(sys.argv)):
+		product*=int(sys.argv[i])
+	print "Product is " +str(product) 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    main()
